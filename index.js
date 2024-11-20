46d4bde0ac8ba20ec61d49253936036420cec3cb
Newton Manyisa <manyisanewton26@gmail.com>
2024-11-20 08:40:12 +0300

    phase-0-css-graffiti-lab

---
+++ index.js
@@ -0,0 +1,14 @@
+let a=4.9
+let b="hey newty how are you doing"
+let c='hey newty how are you doing'
+false
+let fruits = [
+    'aple','banana','kiwi','maddy',2,false,3,43,true,{
+        name:"newty",name:"manyisa"
+    }
+    const people = [
+        {
+            name:"newty"
+        }
+    ]
+]
